82097106f192df49ea433548914e3b200b88a200
Sandeep Prakash <Sandeep.Prakash@hotmail.com>
2025-04-08 04:00:50 +0200

    Initial commit of Coming Soon page

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="coming-soon-container">
+      <div className="logo-container">
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Coming Soon!</h1><br />
+      <p>I'm The Best</p>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
